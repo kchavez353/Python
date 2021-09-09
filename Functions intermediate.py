1c71fa0a978c90bad3259286f402a2ac634927e6
Keavagh Chavez <80922170+kchavez353@users.noreply.github.com>
2021-09-09 14:37:52 -0700

    Add files via upload

---
+++ Functions intermediate.py
@@ -0,0 +1,58 @@
+num = [ [5,2,3], [10,8,9] ]
+
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+x[1][0]=15
+print(x)
+
+student [0]['last_name'] = "Bryant"
+print(students
+)
+sports_directory['soccer'][0]='Andres'
+print(sports_directory)
+
+z[0]['y'] = 30
+print(z)
+
+students = [
+         {'first_name':  'Michael', 'last_name' : 'Jordan'},
+         {'first_name' : 'John', 'last_name' : 'Rosales'},
+         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+         {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+def iterateDictionary(students):
+    for student in students:
+        print(f"first_name - {student['first_name']}, last_name - {student['last_name']}")
+
+iterateDictionary(students)
+
+def iterateDictionary2(key_name, some_list):
+    for student in some_list:
+        print(student[key_name])
+
+iterateDictionary2('first_name', students)
+iterateDictionary2('last_name', students)
+
+dojo = {
+   'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+   'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+def printInfo(dojo):
+    for key in dojo:
+        print(len(dojo[key]), key)
+
+        for entry in range(len(dojo[key])):
+            print(dojo[key][entry])
+
+printInfo(dojo)
+
+
